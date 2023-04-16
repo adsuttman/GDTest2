507630aa2ed658903ca489372088495abb8196df
adsuttman <adsuttman@gmail.com>
2023-04-16 08:57:09 -0400

    added player to player detection field signal

--- Resources/Components/player_detection_field.gd
+++ Resources/Components/player_detection_field.gd
@@ -1,16 +1,16 @@
 extends Area2D
 class_name PlayerDetectionField
 
-signal player_entered
-signal player_exited
+signal player_entered(player: Player)
+signal player_exited(player: Player)
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		player_entered.emit()
+		player_entered.emit(body)
 
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		player_exited.emit()
+		player_exited.emit(body)
