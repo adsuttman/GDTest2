5f374f693fe73dde949409ae9653cd6c758e85d0
adsuttman <adsuttman@gmail.com>
2023-04-16 08:36:54 -0400

    created player detector component

---
+++ Resources/Components/player_detection_field.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+signal player_entered
+signal player_exited
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_entered.emit()
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_exited.emit()
